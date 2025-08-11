2f876f43badd77342dbe501b7132b02201c463c5
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-11 13:56:11 -0700

    gradient text for header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Navbar } from '@/components/index';
+import GradientText from './GradientText'
 
 const Header = () => {
   return (
     <div className='bg-amber-50'>
         <div className='flex px-2 sm:px-6 lg:px-8 font-decorative justify-center p-10'>
-            <h1 className='m-auto text-[50px] md:text-[150px]'>
+            <GradientText 
+            colors={["#000000", "#000000", "#910000", "#000000", "#000000"]} // #fffbeb #910000
+            animationSpeed={20}
+            showBorder={false}
+            className="m-auto text-[50px] md:text-[150px]"
+            >
                 Alan + Doli
-            </h1>
+            </GradientText>
         </div>
         <Navbar />
     </div>
--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Navbar } from './Navbar';
+export { default as GradientText } from './GradientText';
 export { default as Landing } from './Landing';
