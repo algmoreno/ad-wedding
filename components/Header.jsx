14bde58213043ac50def0c0973b83899ec474fdc
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-11 16:34:31 -0700

    landing background with save date card blur

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import GradientText from './GradientText'
 
 const Header = () => {
   return (
-    <div className='bg-amber-50'>
+    <div className='bg-transparent'>
         <div className='flex px-2 sm:px-6 lg:px-8 font-decorative justify-center p-10'>
             <GradientText 
             colors={["#000000", "#000000", "#ba1004", "#000000", "#000000"]} // #fffbeb #910000
--- components/SaveDateCard.jsx
+++ components/SaveDateCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const SaveDateCard = () => {
   return (
-    <div className="flex bg-0 max-w-3xl h-[600px] m-auto overflow-hidden shadow-sm font-decorative drop-shadow-xl/50">
-        <div className="bg-white grid grid-cols-1 grid-rows-3 m-auto gap-4 px-4 py-5 sm:p-6 border-4 border-black backdrop-blur-lg backdrop-brightness-[120%] ">
+    <div className="flex bg-2 max-w-3xl h-[600px] m-auto overflow-hidden shadow-sm font-decorative drop-shadow-xl/100 ">
+        <div className="bg-red-900/60 backdrop-blur-sm grid grid-cols-1 grid-rows-3 m-auto gap-4 px-4 py-5 sm:p-6 border-4 border-black ">
             <h2 className='text-4xl m-auto'>
                 Saturday, April 18, 2026
             </h2>
