d1d3404b4d2720693dcc72b0a547964852df4982
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-11 17:20:41 -0700

    working on cut off text in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,15 +4,15 @@ import GradientText from './GradientText'
 
 const Header = () => {
   return (
-    <div className='bg-transparent'>
+    <div className='bg-white'>
         <div className='flex px-2 sm:px-6 lg:px-8 font-decorative justify-center p-10'>
             <GradientText 
-            colors={["#000000", "#000000", "#ba1004", "#000000", "#000000"]} // #fffbeb #910000
-            animationSpeed={10}
+            colors={["#000000", "#000000", "#ba1004", "#000000", "#ba1004"]} // #fffbeb #910000
+            animationSpeed={20}
             showBorder={false}
             className="m-auto text-[40px] md:text-[125px]"
             >
-                Alan + Doli
+                Alan & Doli
             </GradientText>
         </div>
         <Navbar />
