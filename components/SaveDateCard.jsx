12fd307c378f0822c54a642df2c8341adc35ea8f
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-12 13:55:33 -0700

    font change

--- components/SaveDateCard.jsx
+++ components/SaveDateCard.jsx
@@ -3,9 +3,9 @@ import Balatro from './Balatro';
 
 const SaveDateCard = () => {
   return (
-    <div className="bg-6 flex max-w-3xl h-[600px] m-auto overflow-hidden shadow-sm font-decorative drop-shadow-xl/100 ">
+    <div className="bg-6 flex max-w-3xl h-[600px] m-auto overflow-hidden shadow-sm font-decorative drop-shadow-xl/50 rounded-sm">
         <div className="relative w-full bg-6">
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-gray-800/70 text-shadow-lg/40 text-shadow-white text-6xl">
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-gray-800/90 text-shadow-lg/80 text-shadow-white text-6xl">
             <Balatro isRotate={false} mouseInteraction={false} pixelFilter={2000} />
             <div className="absolute inset-0 from-custom pointer-events-none text-center" />
                 <h2 className="z-10 mb-10">Saturday, April 18, 2026</h2>
