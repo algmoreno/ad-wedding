f567cb2a4a905d6c6528f6ec53586376a37464df
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-14 16:46:30 -0700

    style checkpoint

--- components/Balatro.jsx
+++ components/Balatro.jsx
@@ -107,9 +107,9 @@ void main() {
 
 export default function Balatro({
   spinRotation = -2.0,
-  spinSpeed = 1.5,
+  spinSpeed = 1.0,
   offset = [0.0, 0.0],
-  color1 = "#eeeeee", // outer #011433
+  color1 = "#ffffff", // outer #011433
   color2 = "#034f91", // center #d11c00
   color3 = "#eeeeee", 
   contrast = 3.5,
