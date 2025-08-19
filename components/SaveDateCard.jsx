c36f90dba8992a7d19ef38d3d768bddad18bc33c
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-19 16:10:16 -0700

    changed mobile font size

--- components/SaveDateCard.jsx
+++ components/SaveDateCard.jsx
@@ -9,9 +9,9 @@ const SaveDateCard = () => {
             <div className="absolute inset-0 flex flex-col items-center justify-center ">
             <Balatro isRotate={false} mouseInteraction={false} pixelFilter={2000} />
             <div className="absolute inset-0 balatro-custom pointer-events-none" />
-                <h2 className="text-3xl sm:text-7xl z-10 m-10 text-gray-800/90 text-shadow-lg/80 text-shadow-white text-center">Saturday, April 18, 2026</h2>
-                <h2 className="text-3xl sm:text-5xl z-10 m-10 font-header text-gray-900 text-shadow-lg text-shadow-white text-center">Hilton El Conquistador Resort</h2>
-                <h1 className="text-3xl sm:text-5xl z-10 m-10 font-header text-gray-900 text-shadow-lg text-shadow-white text-center">Tucson, AZ</h1>
+                <h2 className="text-5xl sm:text-7xl z-10 m-10 text-gray-800/90 text-shadow-lg/80 text-shadow-white text-center">Saturday, April 18, 2026</h2>
+                <h2 className="text-4xl sm:text-5xl z-10 m-10 font-header text-gray-900 text-shadow-lg text-shadow-white text-center">Hilton El Conquistador Resort</h2>
+                <h1 className="text-4xl sm:text-5xl z-10 m-10 font-header text-gray-900 text-shadow-lg text-shadow-white text-center">Tucson, AZ</h1>
             </div>
 
         </div>
