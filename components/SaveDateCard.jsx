6a45856085dea3167cff9c3922dde375939bbf31
Alan Moreno <amoreno@perceptivesensors.com>
2025-08-19 16:34:19 -0700

    fonts

--- components/SaveDateCard.jsx
+++ components/SaveDateCard.jsx
@@ -9,9 +9,9 @@ const SaveDateCard = () => {
             <div className="absolute inset-0 flex flex-col items-center justify-center ">
             <Balatro isRotate={false} mouseInteraction={false} pixelFilter={2000} />
             <div className="absolute inset-0 balatro-custom pointer-events-none" />
-                <h2 className="text-5xl font-extrabold sm:text-7xl z-10 m-10 text-black/90 text-shadow-lg text-shadow-blue-400/40 text-center">Saturday, April 18, 2026</h2>
-                <h2 className="text-4xl font-extrabold sm:text-5xl z-10 m-10 text-black text-shadow-lg text-shadow-gray-500/50 text-center">Hilton El Conquistador Resort</h2>
-                <h1 className="text-4xl font-extrabold sm:text-5xl z-10 m-10 text-black text-shadow-lg text-shadow-gray-500/50 text-center">Tucson, AZ</h1>
+                <h2 className="text-5xl font-extrabold sm:text-7xl z-10 m-10 text-black/90 text-shadow-lg text-shadow-amber-900/70 text-center">Saturday, April 18, 2026</h2>
+                <h2 className="text-4xl font-extrabold sm:text-5xl z-10 m-10 text-black text-shadow-lg text-shadow-blue-400/40 text-center">Hilton El Conquistador Resort</h2>
+                <h1 className="text-4xl font-extrabold sm:text-5xl z-10 m-10 text-black text-shadow-lg text-shadow-blue-400/40 text-center">Tucson, AZ</h1>
             </div>
 
         </div>
