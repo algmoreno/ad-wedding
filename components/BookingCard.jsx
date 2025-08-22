bbdb151c4968f2705dd4af78b1217ffb75211b87
algmoreno <alg.moreno00@gmail.com>
2025-08-21 22:43:53 -0700

    updates

--- components/BookingCard.jsx
+++ components/BookingCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const BookingCard = () => {
   return (
-    <div id="booking" className="bg-6-1 mt-20 flex h-[700px] m-auto overflow-hidden shadow-sm font-primary drop-shadow-xl/50 rounded-sm border-8 border-double border-white">
+    <div id="booking" className="flex bg-6-1 mt-20 h-[700px] max-w-[1300px] m-auto overflow-hidden shadow-sm font-primary drop-shadow-xl/50 rounded-sm border-8 border-double border-white">
         <div className="relative w-full">
-            <div className="absolute w-[85%] bg-6-1 inset-0 flex flex-col px-5 items-center justify-center text-white backdrop-blur-4xl drop-shadow-xl/90 text-center">
+            <div className="absolute w-3xl bg-6-1 inset-0 flex flex-col px-5 items-center justify-center text-white backdrop-blur-4xl drop-shadow-xl/90 text-center">
                 <h2 className="z-10 m-10 text-2xl sm:text-4xl text-shadow-sm/60 text-shadow-black">Travel & Accommodation Information:</h2>
                 <h2 className="z-10 mt-10 text-md sm:text-2xl underline decoration-white text-shadow-lg/60 text-shadow-black">Hilton El Conquistador Resort</h2>
                 <h2 className="z-10 mt-1 text-md sm:text-2xl text-shadow-lg/60 text-shadow-black">10000 N Oracle Rd, Tucson, AZ 85704</h2>
