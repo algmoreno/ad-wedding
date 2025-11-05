3b14697a740974df259b5fa6a5ec37b2cdd0e150
Alan Moreno <amoreno@perceptivesensors.com>
2025-11-05 16:12:18 -0700

    getting party id from params

--- components/RsvpForm.jsx
+++ components/RsvpForm.jsx
@@ -1,6 +1,20 @@
+'use client'
 import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
 
-const RsvpForm = () => {
+const RsvpForm = ({ params }) => {
+  const { id } = useParams();
+  // get party details
+  useEffect(() => {
+    if (id) {
+      console.log(id)
+      // axios.get(`/api/auth/appointments/${id}`)
+      //   .then(res =>{setAppointment(res.data.appointment)})
+      //   .catch(err => console.error(err));
+    }
+  }, [id]);
+
   return (
     <div id="booking" className="flex mt-20 h-[1000px] max-w-6xl m-auto">
       RSVP
