b7a22326c430bdf0c63f257f4d8ba3432843e87a
Alan Moreno <amoreno@perceptivesensors.com>
2025-11-05 17:22:32 -0700

    commented routes for now

--- app/api/auth/party/[id]/route.js
+++ app/api/auth/party/[id]/route.js
@@ -1,62 +1,62 @@
-import { ObjectId } from "mongodb";
-import mongoClient from "@/app/lib/mongodb";
-import Appointment from "@/app/models/Appointment";
-import User from "@/app/models/User";
-import { NextResponse } from 'next/server';
+// import { ObjectId } from "mongodb";
+// import mongoClient from "@/app/lib/mongodb";
+// import Appointment from "@/app/models/Appointment";
+// import User from "@/app/models/User";
+// import { NextResponse } from 'next/server';
 
-// Get user appointments
-export async function GET(req, context) {
-  const params = await context.params;
-  try {
-    await mongoClient();
-    const appointment = await Appointment.findById(params.id).populate("user");
+// // Get user appointments
+// export async function GET(req, context) {
+//   const params = await context.params;
+//   try {
+//     await mongoClient();
+//     const appointment = await Appointment.findById(params.id).populate("user");
 
-    if (!appointment) {
-      return NextResponse.json({ message: "Appointment not found" }, { status: 404 });
-    }
+//     if (!appointment) {
+//       return NextResponse.json({ message: "Appointment not found" }, { status: 404 });
+//     }
 
-    return NextResponse.json({ appointment }, { status: 201 });
-  } catch (err) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ appointment }, { status: 201 });
+//   } catch (err) {
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
 
-// Edit appointment
-export async function PUT(req, context) {
-  const params = await context.params;
-  try {
-    await mongoClient();
-    const update = await req.json();
-    const updatedAppointment = await Appointment.findByIdAndUpdate(params.id, update, { new: true });
+// // Edit appointment
+// export async function PUT(req, context) {
+//   const params = await context.params;
+//   try {
+//     await mongoClient();
+//     const update = await req.json();
+//     const updatedAppointment = await Appointment.findByIdAndUpdate(params.id, update, { new: true });
 
-    if (!updatedAppointment) {
-      return NextResponse.json({ message: "Appointment not found" }, { status: 404 });
-    }
+//     if (!updatedAppointment) {
+//       return NextResponse.json({ message: "Appointment not found" }, { status: 404 });
+//     }
 
-    return NextResponse.json({ message: "Appointment updated" }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ message: "Appointment updated" }, { status: 200 });
+//   } catch (err) {
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
 
-// Delete appointment
-export async function DELETE(req, context) {
-  const params = await context.params;
-  try {
-    await mongoClient();
-    const { userId } = await req.json();
-    const deletedAppointment = await Appointment.findByIdAndDelete(params.id);
+// // Delete appointment
+// export async function DELETE(req, context) {
+//   const params = await context.params;
+//   try {
+//     await mongoClient();
+//     const { userId } = await req.json();
+//     const deletedAppointment = await Appointment.findByIdAndDelete(params.id);
 
-    if (!deletedAppointment) {
-      return NextResponse.json({ message: "Appointment not found" }, { status: 404 });
-    }
+//     if (!deletedAppointment) {
+//       return NextResponse.json({ message: "Appointment not found" }, { status: 404 });
+//     }
 
-    await User.findByIdAndUpdate(userId, {
-      $pull: { appointments: params.id }
-    });
+//     await User.findByIdAndUpdate(userId, {
+//       $pull: { appointments: params.id }
+//     });
 
-    return NextResponse.json({ message: "Appointment deleted" }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ message: "Appointment deleted" }, { status: 200 });
+//   } catch (err) {
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
--- app/api/auth/party/route.js
+++ app/api/auth/party/route.js
@@ -1,40 +1,40 @@
-import { ObjectId } from "mongodb";
-import mongoClient from "@/app/lib/mongodb";
-import Appointment from "@/app/models/Appointment";
-import User from "@/app/models/User";
-import { NextResponse } from 'next/server';
+// import { ObjectId } from "mongodb";
+// import mongoClient from "@/app/lib/mongodb";
+// import Appointment from "@/app/models/Appointment";
+// import User from "@/app/models/User";
+// import { NextResponse } from 'next/server';
 
-// Create new appointment
-export async function POST(req){
-  try {
-    await mongoClient(); 
-    const { userId, startDatetime, endDatetime, price, service } = await req.json();
+// // Create new appointment
+// export async function POST(req){
+//   try {
+//     await mongoClient(); 
+//     const { userId, startDatetime, endDatetime, price, service } = await req.json();
 
-    // Find the user
-    const user = await User.findById(userId);
-    if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
-    }
-    //creating new appointment 
-    const newAppointment = await Appointment.create({ user: userId, startDatetime, endDatetime, price, service }); 
+//     // Find the user
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return NextResponse.json({ error: "User not found" }, { status: 404 });
+//     }
+//     //creating new appointment 
+//     const newAppointment = await Appointment.create({ user: userId, startDatetime, endDatetime, price, service }); 
 
-    // pushing appointment object ref to user appointment array
-    user.appointments.push(newAppointment._id);
-    await user.save();
+//     // pushing appointment object ref to user appointment array
+//     user.appointments.push(newAppointment._id);
+//     await user.save();
 
-    return NextResponse.json({ message: "Appointment added!" }, { status: 201 });
-  } catch (err) {
-    return NextResponse.json({ error: err.message, status: 500 });
-  }
-}
+//     return NextResponse.json({ message: "Appointment added!" }, { status: 201 });
+//   } catch (err) {
+//     return NextResponse.json({ error: err.message, status: 500 });
+//   }
+// }
 
-// Get all parties 
-export async function GET() {
-  try {
-    await mongoClient();
-    const appointments = await Appointment.find().populate("user");
-    return NextResponse.json({ appointments },  { status: 201 });
-  } catch (err) {
-    return new NextResponse.json({ error: err.message, status: 500 });
-  }
-}
+// // Get all parties 
+// export async function GET() {
+//   try {
+//     await mongoClient();
+//     const appointments = await Appointment.find().populate("user");
+//     return NextResponse.json({ appointments },  { status: 201 });
+//   } catch (err) {
+//     return new NextResponse.json({ error: err.message, status: 500 });
+//   }
+// }
