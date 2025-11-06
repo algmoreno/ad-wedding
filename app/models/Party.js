8a3687c30999e0949cb6c84e26b14040d0574f02
Alan Moreno <amoreno@perceptivesensors.com>
2025-11-05 17:18:34 -0700

    added member and party to models; rsvp form page

--- app/models/Party.js
+++ app/models/Party.js
@@ -1,11 +1,8 @@
 import { Schema, model, models } from 'mongoose';
 
 const partySchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  startDatetime: { type: Date, required: true, unique: true },
-  endDatetime: { type: Date, required: true, unique: true },
-  service: { type: String, required: true },
-  price: { type: Number, required: true }
+  partyId: { type: String, required: true },
+  members: [{ type: Schema.Types.ObjectId, ref: "Member" }],
 })
 
 const Party = models.Party || model("Party", partySchema);
