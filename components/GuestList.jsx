9debd351662b7b5bbfd5c6450b4d711ab1b52c5e
Alan Moreno <amoreno@perceptivesensors.com>
2025-11-06 15:15:35 -0700

    guest list populating from db; totals tally box; styling

--- components/GuestList.jsx
+++ components/GuestList.jsx
@@ -1,20 +1,30 @@
 'use client'
+import axios from 'axios';
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const GuestList = () => {
-  const people = [
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Courtney Henry', title: 'Designer', email: 'courtney.henry@example.com', role: 'Admin' },
-    { name: 'Tom Cook', title: 'Director of Product', email: 'tom.cook@example.com', role: 'Member' },
-    { name: 'Whitney Francis', title: 'Copywriter', email: 'whitney.francis@example.com', role: 'Admin' },
-    { name: 'Leonard Krasner', title: 'Senior Designer', email: 'leonard.krasner@example.com', role: 'Owner' },
-    { name: 'Floyd Miles', title: 'Principal Designer', email: 'floyd.miles@example.com', role: 'Member' },
-  ]
+  const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // get all members
+  useEffect(() => {
+    axios.get(`/api/auth/member/`)
+      .then(res =>{console.log(res);setMembers(res.data.Members); setLoading(false)})
+      .catch(err => console.error(err));
+  }, []);
+
+  // Make names uppercase 
+  function capitalizeFirstLetter(name) {
+    if (typeof name !== 'string' || name.length === 0) {
+      return name; // Handle non-string or empty inputs
+    }
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
 
   return (
     <div>
-      <div className="mt-8 flow-root overflow-hidden bg-white border-2 m-2">
+      <div className="mt-8 flow-root overflow-hidden bg-white border-2 m-2 font-business">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <table className="w-full text-left">
             <thead className="bg-white">
@@ -28,16 +38,19 @@ const GuestList = () => {
                   scope="col"
                   className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
                 >
-                  Title
+                  Friday Dinner
                 </th>
                 <th
                   scope="col"
                   className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 md:table-cell"
                 >
-                  Email
+                  Ceremony & Lunch
                 </th>
                 <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  Role
+                  Reception
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                  Dietary Restrictions
                 </th>
                 <th scope="col" className="py-3.5 pl-3">
                   <span className="sr-only">Edit</span>
@@ -45,27 +58,41 @@ const GuestList = () => {
               </tr>
             </thead>
             <tbody>
-              {people.map((person) => (
-                <tr key={person.email}>
+              {members.map((member) => (
+                <tr key={member.email}>
                   <td className="relative py-4 pr-3 text-sm font-medium text-gray-900">
-                    {person.name}
+                    {capitalizeFirstLetter(member.firstName)} {capitalizeFirstLetter(member.lastName)}
                     <div className="absolute right-full bottom-0 h-px w-screen bg-gray-100" />
                     <div className="absolute bottom-0 left-0 h-px w-screen bg-gray-100" />
                   </td>
-                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{person.title}</td>
-                  <td className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell">{person.email}</td>
-                  <td className="px-3 py-4 text-sm text-gray-500">{person.role}</td>
-                  <td className="py-4 pl-3 text-right text-sm font-medium">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      Edit<span className="sr-only">, {person.name}</span>
-                    </a>
-                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{member.attendingFriday}</td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{member.attendingCeremony}</td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{member.attendingReception}</td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell">{member.dietaryRestrictions}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
       </div>
+      <div className="flex p-2 border-1 gap-6 items-center font-business bg-black ">
+          <div className="m-auto">
+            <div className="flex gap-20 text-blue-500">
+            <h1 className="text-xl">
+              Totals
+            </h1>
+              <h1>
+                Friday: 
+              </h1>
+              <h1>
+                Ceremony & Lunch: 
+              </h1>
+              <h1>
+                Reception: 
+              </h1>
+            </div>
+          </div>
+        </div>
     </div>
   )
  }
--- components/NewPartyForm.jsx
+++ components/NewPartyForm.jsx
@@ -30,7 +30,7 @@ const NewPartyForm = () => {
 
   return (
     <>
-      <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8 font-business">
         <div className="w-full max-w-sm space-y-10 border-2 p-10">
           <div>
             <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
