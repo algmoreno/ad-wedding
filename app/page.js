4606d6ab36aa38508b585ad5813ed108cf610a5a
Alan Moreno <amoreno@perceptivesensors.com>
2025-11-24 09:31:07 -0700

    google search entry fix attempt in layout

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import {EventDetailsCard, HeaderLanding } from '@/components/index';
 import Balatro from '../components/Balatro';
 
+export const metadata = {
+  title: "D+A",
+  description: "Doli and Alan Wedding",
+};
+
 export default function Home() {
   return (
     <div className=''>
